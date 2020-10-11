98c13d14c7a8260816288320a451d9ddb7288079
Hannes Mann <hannes@computer.localdomain>
2020-10-11 23:27:00 +0200

    Restore the old bInterval value on exit

--- gcadapter_oc.c
+++ gcadapter_oc.c
@@ -12,9 +12,14 @@ MODULE_DESCRIPTION("Filter kernel module to set the polling rate of the Wii U/Ma
 MODULE_VERSION("1.0");
 
 static struct usb_device* adapter_device = NULL;
-static unsigned short rate = 2;
 
-static void patch_endpoints(void) {
+static unsigned short restore_interval = 8;
+static unsigned short configured_interval = 2;
+
+/* Patches all applicable endpoints. Returns the bInterval value used before patching. */
+static unsigned short patch_endpoints(unsigned short interval) {
+	static unsigned short old_interval = 8;
+
 	if(adapter_device != NULL && adapter_device->actconfig != NULL) {
 		struct usb_interface* interface = adapter_device->actconfig->interface[0];
 		
@@ -24,7 +29,8 @@ static void patch_endpoints(void) {
 			
 				for(__u8 endpoint = 0; endpoint < altsettingptr->desc.bNumEndpoints; endpoint++) {
 					if(altsettingptr->endpoint[endpoint].desc.bEndpointAddress == 0x81 || altsettingptr->endpoint[endpoint].desc.bEndpointAddress == 0x02) {
-						altsettingptr->endpoint[endpoint].desc.bInterval = rate;
+						old_interval = altsettingptr->endpoint[endpoint].desc.bInterval;
+						altsettingptr->endpoint[endpoint].desc.bInterval = interval;
 					}
 				}
 			}
@@ -32,6 +38,8 @@ static void patch_endpoints(void) {
 		
 		usb_reset_device(adapter_device);
 	}
+
+	return old_interval;
 }
 
 static int on_usb_notify(struct notifier_block* self, unsigned long action, void* dev) {
@@ -43,7 +51,7 @@ static int on_usb_notify(struct notifier_block* self, unsigned long action, void
 				adapter_device = device;
 				printk(KERN_INFO "gcadapter_oc: Adapter connected\n");
 				
-				patch_endpoints();
+				restore_interval = patch_endpoints(configured_interval);
 			}
 			break;
 			
@@ -65,21 +73,21 @@ static int bus_device_cb(struct usb_device* device, void* data) {
 		adapter_device = device;
 		printk(KERN_INFO "gcadapter_oc: Adapter connected\n");
 				
-		patch_endpoints();
+		restore_interval = patch_endpoints(configured_interval);
 	}
 	return 0;
 }
 
 extern struct bus_type usb_bus_type;
 static int __init on_module_init(void) {
-	if(rate > 255) {
-		printk(KERN_WARNING "gcadapter_oc: Invalid rate parameter specified.\n");
-		rate = 255;
+	if(configured_interval > 255) {
+		printk(KERN_WARNING "gcadapter_oc: Invalid interval parameter specified.\n");
+		configured_interval = 255;
 	}
 	
-	if(rate == 0) {
-		printk(KERN_WARNING "gcadapter_oc: Invalid rate parameter specified.\n");
-		rate = 1;
+	if(configured_interval == 0) {
+		printk(KERN_WARNING "gcadapter_oc: Invalid interval parameter specified.\n");
+		configured_interval = 1;
 	}
     	
 	usb_for_each_dev(NULL, &bus_device_cb);
@@ -90,8 +98,7 @@ static int __init on_module_init(void) {
 
 static void __exit on_module_exit(void) {
 	if(adapter_device != NULL) {
-		rate = 8;
-		patch_endpoints();
+		patch_endpoints(restore_interval);
 	}
 	
 	usb_unregister_notify(&usb_nb);
@@ -100,30 +107,30 @@ static void __exit on_module_exit(void) {
 module_init(on_module_init);
 module_exit(on_module_exit);
 
-static int on_rate_changed(const char* value, const struct kernel_param* kp) {
+static int on_interval_changed(const char* value, const struct kernel_param* kp) {
 	int res = param_set_ushort(value, kp);
 
     if(res == 0) {		
-    	if(rate > 255) { 
-			printk(KERN_WARNING "gcadapter_oc: Invalid rate parameter specified.\n");
-			rate = 255; 
+    	if(configured_interval > 255) { 
+			printk(KERN_WARNING "gcadapter_oc: Invalid interval parameter specified.\n");
+			configured_interval = 255; 
 		}
-    	else if(rate == 0) { 
-			printk(KERN_WARNING "gcadapter_oc: Invalid rate parameter specified.\n");
-			rate = 1; 
+    	else if(configured_interval == 0) { 
+			printk(KERN_WARNING "gcadapter_oc: Invalid interval parameter specified.\n");
+			configured_interval = 1; 
 		}
     	
-		patch_endpoints();
+		patch_endpoints(configured_interval);
     }
 
 	return res;
 }
 
-static struct kernel_param_ops rate_ops =
+static struct kernel_param_ops interval_ops =
 {
-    .set = &on_rate_changed,
+    .set = &on_interval_changed,
     .get = &param_get_ushort
 };
 
-module_param_cb(rate, &rate_ops, &rate, 0644);
-MODULE_PARM_DESC(rate, "Polling rate (default: 2)");
+module_param_cb(interval, &interval_ops, &configured_interval, 0644);
+MODULE_PARM_DESC(interval, "Polling rate (default: 2)");
